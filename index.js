01f6a4ec38bb5c133c020a8a8d020e020c139e1d
kijae <kimkijea7619@hanyang.ac.kr>
2024-05-14 19:09:34 +0900

    FEAT) all-users

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
-import { ping } from './controller/system.js';
 
-const serverPort = 3000;
+import { ping } from './controller/system.js';
+import { getAllUsers } from './lib/userInfo.js';
+
+const serverPort =  3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 const router = express.Router();
 
-
 router.route('/ping').get(ping);
+router.route('/all-users').get(getAllUsers);
 
 app.use('/socialLoginProject/api/v1',router);
 app.listen(serverPort);
-
-//http://51.20.71.68:3000/socialLoginProject/api/v1/pin 
+// http://localhost:3000/socialLoginProject/api/v1/ping
--- sql/create.sql
+++ sql/create.sql
@@ -1,40 +1,33 @@
-CREATE DATABASE DB_TABLES CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE User(
+DROP SCHEMA IF EXISTS socialLoginProject;
+CREATE SCHEMA IF NOT EXISTS socialLoginProject DEFAULT CHARACTER SET utf8;
+USE socialLoginProject;
+
+-- -----------------------------
+
+DROP TABLE IF EXISTS socialLoginProject.user;
+
+CREATE TABLE user (
   userId VARCHAR(60),
-  username VARCHAR(20) CHARACTER SET utf8mb4,
+  userName VARCHAR(20),
   profileImg VARCHAR(200),
   created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  Updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+  updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
   PRIMARY KEY(userId)
 );
 
--- -------------------------------------------------------
+-- -----------------------------
 
-INSERT INTO User(userId, username, profileImg) VALUES
-('asd', "김기재", 'red'),
-('qwe', "박근원", 'blue'),
-('zxc', "강민규", 'green'),
-('jkl', "김재민", 'yellow'),
-('bnm', "이다인", 'black');
+INSERT INTO user (userId, userName, profileImg) VALUES
+("root","근원","asdfgh"),
+("park","공원","qwerty"),
+("rlwo","기재","zxcvbn"),
+("xodn","태우","hjklop"),
+("woals","재민","tyuio");
 
--- -------------------------------------------------------
+-- ----------------------------
 
-SELECT userId, username
-FROM User
-ORDER BY created DESC
-LIMIT 1;
-
--- ------------------------
-SELECT username
-FROM User
-WHERE userId = 'asd';
-
--- ------------------------
-DELETE FROM User
-WHERE username = '박근원';
-
--- ------------------------
-UPDATE User
-SET userId = 'dsa'
-WHERE username = '김기재';
+-- SELECT userId, userName FROM user ORDER BY created LIMIT 1;
+-- SELECT userName FROM user WHERE userId='asd';
+-- DELETE FROM user WHERE userName='박근원';
+-- UPDATE user SET userId='dsa' WHERE userName='김기재';
